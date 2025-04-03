676d1c88684c5a8139bff92e84d988f38fcc9daa
dlnminh12 <dominh01022004@gmail.com>
2025-04-03 19:29:42 +0700

    read data using d3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //toggle sidebar
 var sidebarOpen = false;
 var sidebar = document.getElementById("sidebar");  
@@ -15,3 +16,16 @@ function closeSidebar(){
         sidebarOpen = false;
     }
 }
+
+// Đọc dữ liệu từ project_heart_disease.csv
+d3.csv("./data/project_heart_disease.csv").then(function(data) {
+    data.forEach(d => {
+        d.Age = +d.Age;  // Chuyển đổi từ string sang number
+        d["Blood Pressure"] = +d["Blood Pressure"];  // Chuyển đổi từ string sang number
+        d["Cholesterol Level"] = +d["Cholesterol Level"];  
+        d.BMI = parseFloat(d.BMI);  // Chuyển thành số thực
+    });
+    console.log(data[0]);
+}).catch(function(error) {
+    console.error("Lỗi khi đọc file CSV:", error);
+});
